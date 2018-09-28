38dbc695e60c10ecf934ae8ff9fa985b7344fa6f
linsheng <linsheng@xrjiot.com>
2018-09-28 15:28:02 +0800

    基本网络请求

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,66 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'dart:convert';
+
+//import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(new MyApp());
-
-class ParentWidget extends StatefulWidget {
-  @override
-  _ParentWidgetState createState() => _ParentWidgetState();
-}
-
-class _ParentWidgetState extends State<ParentWidget> {
-  bool _active = false;
-
-  void _handleTapboxChanged(bool newValue) {
-    setState(() {
-      _active = newValue;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: TapboxC(
-        active: _active,
-        onChanged: _handleTapboxChanged,
-      ),
-    );
-  }
-}
-
-//----------------------------- TapboxC ------------------------------
-
-class TapboxC extends StatefulWidget {
-  TapboxC({Key key, this.active: false, @required this.onChanged})
-      : super(key: key);
-
-  final bool active;
-  final ValueChanged<bool> onChanged;
-
-  _TapboxCState createState() => _TapboxCState();
-}
-
-class _TapboxCState extends State<TapboxC> {
-  bool _highlight = false;
-
-  void _handleTapDown(TapDownDetails details) {
-    setState(() {
-      _highlight = true;
-    });
-  }
-
-  void _handleTapUp(TapUpDetails details) {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTapCancel() {
-    setState(() {
-      _highlight = false;
-    });
-  }
-
-  void _handleTap() {
-    widget.onChanged(!widget.active);
-  }
-
-  Widget build(BuildContext context) {
-    // This example adds a green border on tap down.
-    // On tap up, the square changes to the opposite state.
-    return GestureDetector(
-      onTapDown: _handleTapDown, // Handle the tap events in the order that
-      onTapUp: _handleTapUp,     // they occur: down, up, tap, cancel
-      onTap: _handleTap,
-      onTapCancel: _handleTapCancel,
-      child: Container(
-        child: Center(
-          child: Text(
-            widget.active ? 'Active' : 'Inactive',
-            style: TextStyle(fontSize: 32.0, color: Colors.white),
-          ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color:
-          widget.active ? Colors.lightGreen[700] : Colors.grey[600],
-          border: _highlight
-              ? Border.all(
-            color: Colors.teal[700],
-            width: 10.0,
-          )
-              : null,
-        ),
-      ),
-    );
-  }
-}
-
-//------------------------------- MyApp --------------------------------
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Demo'),
-        ),
-        body: Center(
-          child: ParentWidget(),
-        ),
+      title: 'Sample App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: SampleAppPage(),
     );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  List widgets = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    loadData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Sample App"),
+        ),
+        body: ListView.builder(
+            itemCount: widgets.length,
+            itemBuilder: (BuildContext context, int position) {
+              return getRow(position);
+            }));
+  }
+
+  Widget getRow(int i) {
+    return Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Text("Row ${widgets[i]["title"]}")
+    );
+  }
+
+  loadData() async {
+    String dataURL = "https://jsonplaceholder.typicode.com/posts";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      widgets = json.decode(response.body);
+    });
   }
 }
