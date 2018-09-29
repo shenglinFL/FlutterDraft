5cc90c52e8cefba4c498b371176b4d81b30b6c65
linsheng <linsheng@xrjiot.com>
2018-09-29 11:44:50 +0800

    Navigation[导航到新页面并返回](https://flutterchina.club/cookbook/navigation/navigation-basics/)

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,47 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp(
-    items: new List<String>.generate(20, (i) => "Item ${i + 1}"),
+  runApp(new MaterialApp(
+    title: 'Navigation Basics',
+    home: new FirstScreen(),
   ));
 }
 
-class MyApp extends StatelessWidget {
-  final List<String> items;
-
-  MyApp({Key key, @required this.items}) : super(key: key);
-
+class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final title = 'Dismissing Items';
-
-    return new MaterialApp(
-      title: title,
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(title),
-        ),
-        body: new ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-
-            return new Dismissible(
-              // Each Dismissible must contain a Key. Keys allow Flutter to
-              // uniquely identify Widgets.
-              key: new Key(item),
-              // We also need to provide a function that will tell our app
-              // what to do after an item has been swiped away.
-              onDismissed: (direction) {
-                items.removeAt(index);
-
-                Scaffold.of(context).showSnackBar(
-                    new SnackBar(content: new Text("$item dismissed")));
-              },
-              // Show a red background as the item is swiped away
-              background: new Container(color: Colors.red),
-              child: new ListTile(title: new Text('$item')),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('First Screen'),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text('Launch new screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              new MaterialPageRoute(builder: (context) => new SecondScreen()),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Second Screen"),
+      ),
+      body: new Center(
+        child: new RaisedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: new Text('Go back!'),
         ),
       ),
     );
