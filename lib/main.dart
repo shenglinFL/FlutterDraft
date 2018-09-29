10ba849e89a86aa6279ff130e51ad8008ea96459
linsheng <linsheng@xrjiot.com>
2018-09-29 13:49:53 +0800

    [自定义导航栏更多按钮](https://flutterchina.club/catalog/samples/basic-app-bar/)

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,19 @@
 
 import 'package:flutter/material.dart';
 
-class AppBarBottomSample extends StatefulWidget {
+// This app is a stateful, it tracks the user's current choice.
+class BasicAppBarSample extends StatefulWidget {
   @override
-  _AppBarBottomSampleState createState() => new _AppBarBottomSampleState();
+  _BasicAppBarSampleState createState() => new _BasicAppBarSampleState();
 }
 
-class _AppBarBottomSampleState extends State<AppBarBottomSample> with SingleTickerProviderStateMixin {
-  TabController _tabController;
+class _BasicAppBarSampleState extends State<BasicAppBarSample> {
+  Choice _selectedChoice = choices[0]; // The app's "state".
 
-  @override
-  void initState() {
-    super.initState();
-    _tabController = new TabController(vsync: this, length: choices.length);
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  void _nextPage(int delta) {
-    final int newIndex = _tabController.index + delta;
-    if (newIndex < 0 || newIndex >= _tabController.length)
-      return;
-    _tabController.animateTo(newIndex);
+  void _select(Choice choice) {
+    setState(() { // Causes the app to rebuild with the new _selectedChoice.
+      _selectedChoice = choice;
+    });
   }
 
   @override
@@ -36,43 +24,38 @@ class _AppBarBottomSampleState extends State<AppBarBottomSample> with SingleTick
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
-//          title: const Text('AppBar Bottom Widget'),
-          title: new PreferredSize(
-            preferredSize: const Size.fromHeight(100.0),
-            child: Image.asset('images/lake.jpg'),
-          ),
-          leading: new IconButton(
-            tooltip: 'Previous choice',
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () { _nextPage(-1); },
-          ),
+          title: const Text('Basic AppBar'),
           actions: <Widget>[
-            new IconButton(
-              icon: const Icon(Icons.arrow_forward),
-              tooltip: 'Next choice',
-              onPressed: () { _nextPage(1); },
+            new IconButton( // action button
+              icon: new Icon(choices[0].icon),
+              onPressed: () { _select(choices[0]); },
+            ),
+            new IconButton( // action button
+              icon: new Icon(choices[1].icon),
+              onPressed: () { _select(choices[1]); },
+            ),
+            new PopupMenuButton<Choice>( // overflow menu
+              onSelected: _select,
+              itemBuilder: (BuildContext context) {
+                return choices.skip(2).map((Choice choice) {
+                  return new PopupMenuItem<Choice>(
+                    value: choice,
+//                    child: new Text(choice.title),
+                    child: new Row(
+                      children: <Widget>[
+                        new Image.asset('images/lake.jpg'),
+                        new Text(choice.title),
+                      ],
+                    ),
+                  );
+                }).toList();
+              },
             ),
           ],
-          bottom: new PreferredSize(
-            preferredSize: const Size.fromHeight(48.0),
-            child: new Theme(
-              data: Theme.of(context).copyWith(accentColor: Colors.white),
-              child: new Container(
-                height: 48.0,
-                alignment: Alignment.center,
-                child: new TabPageSelector(controller: _tabController),
-              ),
-            ),
-          ),
         ),
-        body: new TabBarView(
-          controller: _tabController,
-          children: choices.map((Choice choice) {
-            return new Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: new ChoiceCard(choice: choice),
-            );
-          }).toList(),
+        body: new Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: new ChoiceCard(choice: _selectedChoice),
         ),
       ),
     );
@@ -86,12 +69,12 @@ class Choice {
 }
 
 const List<Choice> choices = const <Choice>[
-  const Choice(title: 'CAR', icon: Icons.directions_car),
-  const Choice(title: 'BICYCLE', icon: Icons.directions_bike),
-  const Choice(title: 'BOAT', icon: Icons.directions_boat),
-  const Choice(title: 'BUS', icon: Icons.directions_bus),
-  const Choice(title: 'TRAIN', icon: Icons.directions_railway),
-  const Choice(title: 'WALK', icon: Icons.directions_walk),
+  const Choice(title: 'Car', icon: Icons.directions_car),
+  const Choice(title: 'Bicycle', icon: Icons.directions_bike),
+  const Choice(title: 'Boat', icon: Icons.directions_boat),
+  const Choice(title: 'Bus', icon: Icons.directions_bus),
+  const Choice(title: 'Train', icon: Icons.directions_railway),
+  const Choice(title: 'Walk', icon: Icons.directions_walk),
 ];
 
 class ChoiceCard extends StatelessWidget {
@@ -119,5 +102,5 @@ class ChoiceCard extends StatelessWidget {
 }
 
 void main() {
-  runApp(new AppBarBottomSample());
+  runApp(new BasicAppBarSample());
 }
