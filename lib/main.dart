c2b9a421f2c07286258825ae6a66e674574192bb
linsheng <linsheng@xrjiot.com>
2018-09-29 11:47:40 +0800

    [给新页面传值](https://flutterchina.club/cookbook/navigation/passing-data/)

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,79 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
+class Todo {
+  final String title;
+  final String description;
+
+  Todo(this.title, this.description);
+}
 
 void main() {
   runApp(new MaterialApp(
-    title: 'Navigation Basics',
-    home: new FirstScreen(),
+    title: 'Passing Data',
+    home: new TodosScreen(
+      todos: new List.generate(
+        20,
+            (i) => new Todo(
+          'Todo $i',
+          'A description of what needs to be done for Todo $i',
+        ),
+      ),
+    ),
   ));
 }
 
-class FirstScreen extends StatelessWidget {
+class TodosScreen extends StatelessWidget {
+  final List<Todo> todos;
+
+  TodosScreen({Key key, @required this.todos}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('First Screen'),
+        title: new Text('Todos'),
       ),
-      body: new Center(
-        child: new RaisedButton(
-          child: new Text('Launch new screen'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              new MaterialPageRoute(builder: (context) => new SecondScreen()),
-            );
-          },
-        ),
+      body: new ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return new ListTile(
+            title: new Text(todos[index].title),
+            // When a user taps on the ListTile, navigate to the DetailScreen.
+            // Notice that we're not only creating a new DetailScreen, we're
+            // also passing the current todo through to it!
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new DetailScreen(todo: todos[index]),
+                ),
+              );
+            },
+          );
+        },
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo
+  final Todo todo;
+
+  // In the constructor, require a Todo
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    // Use the Todo to create our UI
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Second Screen"),
+        title: new Text("${todo.title}"),
       ),
-      body: new Center(
-        child: new RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: new Text('Go back!'),
-        ),
+      body: new Padding(
+        padding: new EdgeInsets.all(16.0),
+        child: new Text('${todo.description}'),
       ),
     );
   }
