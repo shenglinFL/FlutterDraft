8cf4ff241641ebdcf0769b46fc1e2739b27dd474
linsheng <linsheng@xrjiot.com>
2018-09-29 10:46:03 +0800

    显示来自网上的图片

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,20 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appName = 'Custom Themes';
+    var title = 'Web Images';
 
     return new MaterialApp(
-      title: appName,
-      theme: new ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.lightBlue[800],
-        accentColor: Colors.cyan[600],
-      ),
-      home: new MyHomePage(
-        title: appName,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  MyHomePage({Key key, @required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
-      ),
-      body: new Center(
-        child: new Container(
-          color: Theme.of(context).accentColor,
-          child: new Text(
-            'Text with a background color',
-            style: Theme.of(context).textTheme.title,
-          ),
+      title: title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(title),
         ),
-      ),
-      floatingActionButton: new Theme(
-        data: Theme.of(context).copyWith(accentColor: Colors.yellow),
-        child: new FloatingActionButton(
-          onPressed: null,
-          child: new Icon(Icons.add),
+        body: new Image.network(
+          'https://github.com/flutter/website/blob/master/src/_includes/code/layout/lakes/images/lake.jpg?raw=true',
         ),
       ),
     );
